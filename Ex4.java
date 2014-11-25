d8dee572f0bba9c6c4f013711117ef053747269a
n14002@std.it-college.ac.jp <n14002>
2014-11-25 12:04:33 +0900

    11/25

--- Ex4.java
+++ Ex4.java
@@ -1,35 +1,37 @@
+import java.util.Arrays;
+
+
 class Ex4{
 
-	public int [] ex4_1(int [] a){
-		
-		return a;
+  public int [] ex4_1(int [] a){
+   Arrays.sort(a);
+    return a;
+  }
 
-	}
+  public int [] ex4_2(int [] a){
+    return a;
+  }
 
-	public int [] ex4_2(int [] a){
-		
-		return a;
+  public String [] ex4_3(int [] a){
 
-	}
+    String [] hantei = {"正", "負", "零"};
+    
+    }
 
-	public String [] ex4_3(int [] a){
-		
-		String [] hantei = {"dummy"};
-		
-		return hantei;
 
-	}
 
-	public int ex4_4(int a){
-		
-		return -1;
+  }
 
-	}
+  public int ex4_4(int a){
 
-	public int ex4_5(int a){
-		
-		return -1;
+    return -1;
 
-	}
+  }
+
+  public int ex4_5(int a){
+
+    return -1;
+
+  }
 }
 
